6bd6ca504a533701db86f120aa53083e6aef20ca
RenVit318 <rens@renskievit.com>
2023-03-03 17:04:47 +0100

    Hipparcos Mixing Code

--- src/hipp_preprocess.py
+++ src/hipp_preprocess.py
@@ -13,7 +13,52 @@ import numpy as np
 from astropy.io import fits
 
 def mix_hipparcos():
-    pass
+    """Combine the Hipparcos-1 and Hipparcos-2 catalogues based on the mixing factor from Brandt+21"""
+    hip1 = fits.open('../../data/Hipparcos1.fits')[1].data
+    hip2 = fits.open('../../data/Hipparcos2.fits')[1].data
+    mix_factor = 0.6  # Brandt (2021)
+
+    mix_columns = []
+    hip1_mix = ['ra', 'de', 'pmra', 'pmde', 'plx', 'e_radeg', 'e_dedeg', 'e_pmra', 'e_pmde', 'e_plx', 'dera']
+    hip2_mix = ['ra', 'dec', 'pm_ra', 'pm_de', 'plx', 'e_ra_rad', 'e_de_rad', 'e_pm_ra', 'e_pm_de', 'e_plx', 'ra_dec_corr']
+    extra_data = ['hp_mag', 'b_v', 'v_i']
+
+
+
+    print('Starting Hip1 Indexing')
+    # Match indices on hip because hip1 has 263 more objects than hip2
+    hip1_idx = np.full(hip1.shape[0], True)
+    for i in range(hip1.shape[0]):
+        if hip1['hip'][i] not in hip2['hip']:
+            hip1_idx[i] = False
+            print(f'No match found for hip1={hip1["hip"][i]}')
+
+    hip2_covar = extract_cov(hip2)[:,0,1]
+
+    # Add in hip-id
+    mix_columns.append(fits.Column(name='hip', format='K', array=hip2['hip']))  # Format K = 64bit int
+
+    print('Starting Astrometric Mixing')
+    # Mix the astrometric parameters
+    for i in range(len(hip1_mix)):
+        if hip2_mix[i] == 'ra_dec_corr':
+            mixed_data = mix_factor * hip2_covar + (1 - mix_factor) * hip1[hip1_mix[i]][hip1_idx]
+            mix_columns.append(fits.Column(name='ra_dec_corr', format='D', array=mixed_data))
+        else:
+            mixed_data = mix_factor * hip2[hip2_mix[i]] + (1 - mix_factor) * hip1[hip1_mix[i]][hip1_idx]
+            mix_columns.append(fits.Column(name=hip2_mix[i], format='D', array=mixed_data))  # Format D = double precision float
+
+    print('Starting Photometric Extraction')
+    # Add in photometric parameters. Choose here to pick them from vL
+    for i in range(len(extra_data)):
+        mix_columns.append(fits.Column(name=extra_data[i], format='D', array=hip2[extra_data[i]]))
+
+    print('Starting HDUList Creation and Saving')
+    table_hdu = fits.BinTableHDU.from_columns(mix_columns)
+
+    table_hdu.writeto('../../data/Hipparcos_mix.fits', overwrite=True)
+    print('Process Completed')
+
 
 def extract_cov(tab):
     """Transform the u-values given in Hipparcos-2 (van Leeuwen, 2007) into a complete covariance matrix
@@ -23,56 +68,45 @@ def extract_cov(tab):
     print(f'Converting U -> \Sigma for {num} Hip2 objects')
 
     # Setup arrays and parameters
-    U = np.zeros((num,5,5))
-    sigma = np.zeros((num,5))
+    U = np.zeros((num, 5, 5))
+    sigma = np.zeros((num, 5))
     nobs = tab['Ntr']
     gof = tab['f2']
-    nu = nobs-5
-    Q = nu * (np.sqrt(2./(9.*nu)) * gof + 1 - (2./(9.*nu)))**3.
-    u = np.sqrt(Q/nu) # confusing naming. Change?
-    u_sq = u**2.
+    nu = nobs - 5
+    Q = nu * (np.sqrt(2. / (9. * nu)) * gof + 1 - (2. / (9. * nu))) ** 3.
+    u = np.sqrt(Q / nu)  # confusing naming. Change?
+    u_sq = u ** 2.
 
     # Populate sigma
     sigma_names = ['e_ra_rad', 'e_de_rad', 'e_plx', 'e_pm_ra', 'e_pm_de']
     for i, field in enumerate(sigma_names):
-       sigma[:,i] = tab[field]
+        sigma[:, i] = tab[field]
 
     # Populate U
     u_index = 1
     for j in range(5):
-        for i in range(j+1):
+        for i in range(j + 1):
             U[:, i, j] = tab[f'u{u_index}']
             if i == j:
-                U[:, i, j] *= (u/sigma[:,i])
+                U[:, i, j] *= (u / sigma[:, i])
             u_index += 1
 
     U_transpose = np.transpose(U, axes=(0, 2, 1))
     # In case of a 3D array, a matmul treats it as a stack of matrices and treats
-    # the first index as the stack index. 
+    # the first index as the stack index.
     # N = Normal matrix (notation from Michalik et al. (2014))
     N = np.matmul(U_transpose, U)
-    N_inverse = np.linalg.inv(N) 
+    N_inverse = np.linalg.inv(N)
     covar = np.reshape(u_sq[:, None, None] * N_inverse, N_inverse.shape)
 
-    
-    print(len(covar[:,0,1][np.abs(covar[:,0,1]) > 1e2]))
-    idx = np.argmax(covar[:, 0, 1][covar[:, 0, 1] < 1e10])
-    print(tab['hip'][idx])
-    print(sigma[idx])
-    print(gof[idx], nobs[idx])
-    print(Q[idx], u[idx])
-    print(covar[idx])
-    print(U_transpose[idx])
-    print(N[idx])
-    print(N_inverse[idx])
-    
-    return covar  
+    return covar
 
 
 
 def main():
-    hipp_hdul = fits.open('../../data/Hipparcos2.fits')
-    covar = extract_cov(hipp_hdul[1].data)
+    #hipp_hdul = fits.open('../../data/Hipparcos2.fits')
+    #covar = extract_cov(hipp_hdul[1].data)
+    mix_hipparcos()
 
 if __name__ == '__main__':
     main()
